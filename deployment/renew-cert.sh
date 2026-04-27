60f4e08ed793aa477bbe17293165ab71a90ec45f
dickyfarhansyah <dicky.farhansyah@opusasia.co>
2026-04-27 15:06:19 +0700

    chore(deployment): add error trapping to log any error if happen

--- deployment/renew-cert.sh
+++ deployment/renew-cert.sh
@@ -4,11 +4,13 @@ set -e
 PROJECT_DIR="/home/administrator/loop-lm"
 cd "$PROJECT_DIR"
 
-docker run --rm --network host \
--v "$PROJECT_DIR/deployment/conf:/etc/letsencrypt" \
--v "$PROJECT_DIR/deployment/www:/var/www/certbot" \
-certbot/certbot renew --quiet
+{
+    docker run --rm --network host \
+    -v "$PROJECT_DIR/deployment/conf:/etc/letsencrypt" \
+    -v "$PROJECT_DIR/deployment/www:/var/www/certbot" \
+    certbot/certbot renew --quiet
 
-docker compose -f "$PROJECT_DIR/deployment/docker-compose.yml" --env-file "$PROJECT_DIR/deployment/.env.docker" exec -T nginx nginx -s reload
+    docker compose -f "$PROJECT_DIR/deployment/docker-compose.yml" --env-file "$PROJECT_DIR/deployment/.env.docker" exec -T nginx nginx -s reload
 
-echo "$(date): Cert renewal check finished" >> "$PROJECT_DIR/deployment/looplm-renewal.log"
+    echo "$(date): SUCCESS - Cert renewal check finished"
+} >> "$PROJECT_DIR/deployment/looplm-renewal.log" 2>&1
