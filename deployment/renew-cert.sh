75c874ef4126c7976e0124bf98569d2d77018a2e
dickyfarhansyah <dicky.farhansyah@opusasia.co>
2026-04-27 14:34:50 +0700

    ci/cd(deployment): fix compose file path

--- deployment/renew-cert.sh
+++ deployment/renew-cert.sh
@@ -9,6 +9,6 @@ docker run --rm --network host \
 -v "$PROJECT_DIR/deployment/www:/var/www/certbot" \
 certbot/certbot renew --quiet
 
-docker compose exec -w "/deployment" -T nginx nginx -s reload
+docker compose -f "$PROJECT_DIR/deployment/docker-compose.yml" --env-file "$PROJECT_DIR/deployment/.env.docker" exec -T nginx nginx -s reload
 
 echo "$(date): Cert renewal check finished" >> /var/log/looplm-renewal.log
