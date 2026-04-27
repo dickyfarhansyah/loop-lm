0f594bbaccf2cb151129dea4134a1200e7c7e324
dickyfarhansyah <dicky.farhansyah@opusasia.co>
2026-04-27 14:31:34 +0700

    ci/cd(deployment): add cert renewal script

---
+++ deployment/renew-cert.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+PROJECT_DIR="/home/administrator/loop-lm"
+cd "$PROJECT_DIR"
+
+docker run --rm --network host \
+-v "$PROJECT_DIR/deployment/conf:/etc/letsencrypt" \
+-v "$PROJECT_DIR/deployment/www:/var/www/certbot" \
+certbot/certbot renew --quiet
+
+docker compose exec -w "/deployment" -T nginx nginx -s reload
+
+echo "$(date): Cert renewal check finished" >> /var/log/looplm-renewal.log
